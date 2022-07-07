0e6a8aab61b8d5f15084867cd9546b4021d27b38
sebastianfulga <sebastian.fulga@assist.ro>
2022-07-07 10:37:49 +0300

    Added WelcomeController

---
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Controllers/WelcomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assist.Jully._2022.BE2.Controllers
+{
+    [ApiController, Route("api/[controller]"), Produces("application/json")]
+    public class WelcomeController : Controller
+    {
+        [HttpGet("message")]
+        public IActionResult GetWelcomeMessage()
+        {
+            return new OkObjectResult("Welcome to ASSIST Software !");
+        }
+    }
+}
