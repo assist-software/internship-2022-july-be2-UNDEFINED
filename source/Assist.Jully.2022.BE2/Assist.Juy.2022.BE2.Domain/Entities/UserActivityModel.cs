42929b41668da0d33a4b071d1423de2b4b48e7ab
Adu-AM <andreimiron001@gmail.com>
2022-07-12 18:32:15 +0300

    52879 | implement first version of UserActivityModel

---
+++ source/Assist.Jully.2022.BE2/Assist.Juy.2022.BE2.Domain/Entities/UserActivityModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Assist.Juy_2022.BE2.Domain
+{
+    public record UserActivityModel
+    {
+        string Device { get; set; } 
+        string DeviceType { get; set; }
+        string Location { get; set; }
+        DateTime ConnectionDate { get; set; }
+        enum Status { };
+    }
+}
