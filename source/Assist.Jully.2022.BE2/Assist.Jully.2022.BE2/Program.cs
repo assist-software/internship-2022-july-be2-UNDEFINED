5ece851da988038babdc6479ca13674f939a23a4
portocalab <portocalablue23@gmail.com>
2022-07-12 15:38:01 +0300

    add swagger

--- source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Program.cs
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Program.cs
@@ -3,10 +3,9 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -40,7 +39,6 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
