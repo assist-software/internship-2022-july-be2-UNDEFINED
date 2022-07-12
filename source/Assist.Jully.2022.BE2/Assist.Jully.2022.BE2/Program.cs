0aba402ab9ac8e9785f7e79ee4fd8d08c771073e
Adu-AM <andreimiron001@gmail.com>
2022-07-12 16:15:47 +0300

    52876 | Enable CORS

--- source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Program.cs
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Program.cs
@@ -1,11 +1,23 @@
+var allowSpecificOrigins = "_allowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS with named policy and middleware
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+                      });
+});
 
 var app = builder.Build();
 
@@ -15,6 +27,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// app CORS
+app.UseCors(allowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
