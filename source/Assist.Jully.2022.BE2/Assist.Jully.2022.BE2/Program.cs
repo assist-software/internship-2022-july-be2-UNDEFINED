16a854575f8725243c7b3c654e8e788a4f042b6a
Adu-AM <andreimiron001@gmail.com>
2022-07-12 16:32:39 +0300

    52876 | Fix code edits

--- source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Program.cs
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Program.cs
@@ -1,33 +1,30 @@
-var allowSpecificOrigins = "_allowSpecificOrigins";
+var allowSpecificOrigins = "allowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
-
-
 
 builder.Services.AddControllers();
 
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: allowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+                          policy
+                          .WithOrigins("*")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
                       });
 });
 
 var app = builder.Build();
-
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 
 app.UseCors(allowSpecificOrigins);
 
