ab3ee15b6bebf9c4678d2a53ac5f6e0b30b12c15
Adu-AM <andreimiron001@gmail.com>
2022-07-12 16:21:46 +0300

    52876 | Fix delete comments

--- source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Program.cs
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Program.cs
@@ -1,15 +1,15 @@
 var allowSpecificOrigins = "_allowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Add CORS with named policy and middleware
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: allowSpecificOrigins,
@@ -21,14 +21,14 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// app CORS
+
 app.UseCors(allowSpecificOrigins);
 
 app.UseHttpsRedirection();
