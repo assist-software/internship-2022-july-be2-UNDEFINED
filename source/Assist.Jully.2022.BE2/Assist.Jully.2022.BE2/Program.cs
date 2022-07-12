a3aac9e64dabec56b2830dca6f97f600b637df07
portocalab <portocalablue23@gmail.com>
2022-07-12 14:18:14 +0300

    noua schimbare

--- source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Program.cs
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "version 1", Version = "v1" });
+    c.AddSecurityDefinition("bearer", new OpenApiSecurityScheme()
+    {
+        Name = "authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "bearer token"
+    }); ;
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+            Reference=new OpenApiReference
+            {
+                Type=ReferenceType.SecurityScheme,
+                Id="bearer"
+            }
+
+        },
+        new string[]{}
+        }
+    }
+    );
+});
 
 var app = builder.Build();
 
@@ -16,10 +47,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors();
 
 app.Run();
