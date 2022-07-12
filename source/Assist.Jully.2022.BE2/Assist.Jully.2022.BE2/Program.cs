20c76d0a7f829a8bb0e290b0eea057a3e6277125
Gherghel Alessandro <101932535+gherghelalessandro@users.noreply.github.com>
2022-07-12 16:23:18 +0300

    52877 | add swagger (#4)
    * 52875 | Creare setup initial
    
    * 52875 | Fixed naming convention and deleted obsolete classes
    
    * Rename source/Assist.Jully.2022.BE2/Application/Assist.Juy.2022.BE2.Application.csproj to source/Assist.Jully.2022.BE2/Assist.Juy.2022.BE2.Application/Assist.Juy.2022.BE2.Application.csproj
    
    * Rename source/Assist.Jully.2022.BE2/Domain/Assist.Juy.2022.BE2.Domain.csproj to source/Assist.Jully.2022.BE2/Assist.Juy.2022.BE2.Domain/Assist.Juy.2022.BE2.Domain.csproj
    
    * Rename source/Assist.Jully.2022.BE2/Infrastructure/Assist.Juy.2022.BE2.Infrastructure.csproj to source/Assist.Jully.2022.BE2/Assist.Juy.2022.BE2.Infrastructure/Assist.Juy.2022.BE2.Infrastructure.csproj
    
    * Fixed path issues 0.1
    
    * noua schimbare
    
    * add swagger
    
    Co-authored-by: Adu-AM <andreimiron001@gmail.com>

--- source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Program.cs
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Program.cs
@@ -1,25 +1,55 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "version 1", Version = "v1" });
+    c.AddSecurityDefinition("bearer", new OpenApiSecurityScheme()
+    {
+        Name = "authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "bearer token"
+    }); ;
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+            Reference=new OpenApiReference
+            {
+                Type=ReferenceType.SecurityScheme,
+                Id="bearer"
+            }
+
+        },
+        new string[]{}
+        }
+    }
+    );
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors();
 
 app.Run();
