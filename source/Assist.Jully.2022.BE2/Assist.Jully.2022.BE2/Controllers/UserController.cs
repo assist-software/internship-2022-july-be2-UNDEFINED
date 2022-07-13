ad6230bd9e74020266b9886822dea236c15e0a15
Gherghel Alessandro <101932535+gherghelalessandro@users.noreply.github.com>
2022-07-13 15:08:54 +0300

    add user controller (#8)
    * basic structure
    
    * fix-suggestions
    
    * password fix
    
    * PascalCase

---
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Controllers/UserController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Assist.July._2022.BE2.Domain;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Assist.Jully._2022.BE2.Controllers
+{
+    [Route("api/[controller]"), ApiController, Authorize]
+    public class usercontroller : ControllerBase
+    {
+        
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(UserAccess Access)
+        {
+            try
+            {
+                return new OkObjectResult("ok");
+            }
+            catch
+            {
+                return new BadRequestObjectResult("An error has occured");
+            }
+        }
+        [HttpPost("Register")]
+        public IActionResult Register()
+        {
+            try
+            {
+                return new OkObjectResult("ok");
+            }
+            catch
+            {
+                return new BadRequestObjectResult("An error has occured");
+            }
+        }
+        [HttpPost("reset/password")]
+        public IActionResult resetpassword()
+        {
+            try
+            {
+                return new OkObjectResult("ok");
+            }
+            catch
+            {
+                return new BadRequestObjectResult("An error has occured");
+            }
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id)
+        {
+            try
+            {
+                return new OkObjectResult("ok");
+            }
+            catch
+            {
+                return new BadRequestObjectResult("An error has occured");
+            }
+        }
+        [HttpGet("{id}")]
+        public IActionResult Getuser(int id)
+        {
+            try
+            {
+                return new OkObjectResult("ok");
+            }
+            catch
+            {
+                return new BadRequestObjectResult("An error has occured");
+            }
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return new OkObjectResult("ok");
+            }
+            catch
+            {
+                return new BadRequestObjectResult("An error has occured");
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                return new OkObjectResult("ok");
+            }
+            catch
+            {
+                return new BadRequestObjectResult("An error has occured");
+            }
+        }
+    }
+}
---
+++ source/Assist.Jully.2022.BE2/Assist.July._2022.BE2.Domain/Enums/UserEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assist.July._2022.BE2.Domain
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public enum Role
+    {
+        Admin,
+        Validator,
+        User
+    }
+}
