c2cc98499c5256fa740239cb9d08ad7cfb3a05c5
Marcela <enachiuc.marcela@yahoo.com>
2022-07-13 11:32:45 +0300

    Controllers partial implementation

---
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Controllers/ListingController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+
+
+namespace Assist.Jully._2022.BE2.Controllers
+{
+    [ApiController, Route("api/[controller]"), Produces("application/json")]
+    public class ListingController : Controller
+    {
+        private IMapper mapper;
+
+        [HttpGet("message")]
+        public IActionResult GetMessage()
+        {
+            return new OkObjectResult("Listing Controller!");
+        }
+
+        [HttpPost("Create")]
+        public IActionResult CreateNewListing()
+        {
+            return new OkObjectResult("Create new listing");
+        }
+
+        [HttpGet]
+        public IActionResult GetAllListings()
+        {
+            return new OkObjectResult("Get all listings");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateAListing()
+        {
+            return new OkObjectResult("Update a listing");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAListing()
+        {
+            return new OkObjectResult("Get a listing");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAListing()
+        {
+            return new OkObjectResult("Delete a listing");
+        }
+    }
+}
--- source/Assist.Jully.2022.BE2/Assist.Juy.2022.BE2.Domain/Entities/Listing.cs
+++ source/Assist.Jully.2022.BE2/Assist.Juy.2022.BE2.Domain/Entities/Listing.cs
@@ -5,32 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
-namespace Assist.Juy._2022.BE2.Domain
+namespace Entities
 {
-    public enum Category
+    public record Listing
     {
-        category1,
-        category2
-    }
-    public class Listing
-    {
-        [Required]
         public Guid Id { get; set; }
-        [Required]
         public string Title { get; set; }
-        [Required]
         public string ShortDescription { get; set; }
-        [Required]
         public string Location { get; set; }
-        [Required]
-        public decimal Price { get; set; }
+        public double Price { get; set; }
         public Guid Author { get; set; }
         public Guid ApprovedBy { get; set; }
-        public string Status { get; set; }
-        [Required]
+        public byte Status { get; set; }
         public string Images { get; set; }
+        public string Category { get; set; }
         public int ViewCounter { get; set; }
-        [Required]
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
