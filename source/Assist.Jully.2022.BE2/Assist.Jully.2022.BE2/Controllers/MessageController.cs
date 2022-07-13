8b8fb5803a250d0efea295588b2bf267bfb94c9c
Marcela <enachiuc.marcela@yahoo.com>
2022-07-13 15:52:40 +0300

    Partial implement for message controller

--- source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Controllers/MessageController.cs
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Controllers/MessageController.cs
@@ -5,6 +5,41 @@ namespace Assist.Jully._2022.BE2.Controllers
     [ApiController, Route("api/[controller]"), Produces("application/json")]
     public class MessageController : Controller
     {
-        
+        [HttpPost("{listingId}/{userId}")]
+        public IActionResult SendingNewMessage()
+        {
+            try
+            {
+                return new OkObjectResult("Sending new message");
+            }
+            catch (Exception)
+            {
+                return BadRequest("An error has occured");
+            }
+        }
+        [HttpGet("{listingId}")]
+        public IActionResult GetAllMessages()
+        {
+            try
+            {
+                return new OkObjectResult("Get all messages");
+            }
+            catch (Exception)
+            {
+                return BadRequest("An error has occured");
+            }
+        }
+        [HttpDelete("{listingId}")]
+        public IActionResult DeleteAllMessages()
+        {
+            try
+            {
+                return new OkObjectResult("Delete all messages");
+            }
+            catch (Exception)
+            {
+                return BadRequest("An error has occured");
+            }
+        }
     }
 }
