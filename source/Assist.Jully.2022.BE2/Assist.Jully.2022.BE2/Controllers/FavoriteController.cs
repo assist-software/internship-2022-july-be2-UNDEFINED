92b39eb7cef905ee6d17fb31f9481290217e430d
Adu-AM <andreimiron001@gmail.com>
2022-07-13 14:27:38 +0300

    implemented Favorite entity and controller

---
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Controllers/FavoriteController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assist.Jully._2022.BE2.Controllers
+{
+    [ApiController, Route("/api/favorite"), Produces("application/json")]
+    public class FavoriteController : ControllerBase
+    {
+
+        [HttpPost("{id}")]
+        public IActionResult SaveFavoriteItem()
+        {
+            try
+            {
+                return new OkObjectResult("ok");
+            }
+            catch (Exception)
+            {
+
+                return BadRequest("An error has occured");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetFavorites()
+        {
+            try
+            {
+                return new OkObjectResult("ok");
+            }
+            catch (Exception)
+            {
+
+                return BadRequest("An error has occured");
+            }
+        }
+    }
+}
---
+++ source/Assist.Jully.2022.BE2/Assist.July._2022.BE2.Domain/Entities/Favorite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assist.July._2022.BE2.Domain
+{
+    public record Favorite
+    {
+        public Guid UserId { get; set; }
+        public Guid ListingId { get; set; }
+    }
+}
