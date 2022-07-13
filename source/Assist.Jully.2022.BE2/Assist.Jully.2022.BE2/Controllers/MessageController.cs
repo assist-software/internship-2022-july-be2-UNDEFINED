a3ebd4795e56eda34afe3343300641749bbfecc3
Adu-AM <andreimiron001@gmail.com>
2022-07-13 15:25:12 +0300

    Fix duplicate error

--- source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Controllers/MessageController.cs
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Controllers/MessageController.cs
@@ -5,6 +5,6 @@ namespace Assist.Jully._2022.BE2.Controllers
     [ApiController, Route("api/[controller]"), Produces("application/json")]
     public class MessageController : Controller
     {
-        
+
     }
 }
