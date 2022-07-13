f0c677cdd0253e1c8c138cca92c214a7307309ed
portocalab <portocalablue23@gmail.com>
2022-07-13 17:49:08 +0300

    add controller

---
+++ source/Assist.Jully.2022.BE2/Assist.Jully.2022.BE2/Controllers/MailController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Assist.July._2022.BE2.Domain;
+using Assist.July._2022.BE2.Infrastructure;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MailController : ControllerBase
+{
+    private readonly IMailService mailService;
+    public MailController(IMailService mailService)
+    {
+        this.mailService = mailService;
+    }
+    [HttpPost("send")]
+    public async Task<IActionResult> SendMail([FromForm] MailRequest request)
+    {
+        try
+        {
+            await mailService.SendEmailAsync(request);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+
+    }
+}
