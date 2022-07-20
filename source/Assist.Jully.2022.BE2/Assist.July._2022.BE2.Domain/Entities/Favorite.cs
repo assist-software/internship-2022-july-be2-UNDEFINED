dc2313614bcd365d41da6dd7ce901e3eb31c0102
Marcela <enachiuc.marcela@yahoo.com>
2022-07-20 17:19:34 +0300

    Delete extra fields

--- source/Assist.Jully.2022.BE2/Assist.July._2022.BE2.Domain/Entities/Favorite.cs
+++ source/Assist.Jully.2022.BE2/Assist.July._2022.BE2.Domain/Entities/Favorite.cs
@@ -3,9 +3,6 @@
     public record Favorite
     {
         public Guid Id;
-        public Guid UserId { get; set; }
-        public Guid ListingId { get; set; }
-
         public User? Users { get; set; }
         public Listing? Listings { get; set; }
     }
